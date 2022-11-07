bcf02b36ecfbd04ac5c5d30231b79bce363a56ae
igor-86 <i.campoli86@gmail.com>
2022-11-07 15:39:09 +0100

    Error Components in app vue

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<script>
+
+export default {
+    data() {
+        return {
+            firtMessage: "La mia prima App Vite!"
+        }
+    }
+}
+
+</script>
+
+<template>
+    <h1>{{ firtMessage }}</h1>
+</template>
+
+<style scoped>
+
+</style>
